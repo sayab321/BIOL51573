ad7fe0f90175fea60417eceb18cec255959a5f96
sayab321 <sayabmiya13@gmail.com>
2026-03-04 10:42:22 -0600

    added a greeting, command, and math

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+name = input("enter your name:")
+
+print ("hello world")
+print ('hello world')
+
+print ("hello, ", name, "I hope yoy have a nice day")
+
+#some math
+print (7+7)
